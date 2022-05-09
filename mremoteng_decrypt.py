80ca6d8812d853cea4b52fadb71d8c9173f6a5c1
DidierA <DidierA@lv426>
2022-05-09 18:17:36 +0200

    Add support of "Full file encryption"

--- mremoteng_decrypt.py
+++ mremoteng_decrypt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import csv
 import re
 import base64
 import sys
@@ -9,6 +10,13 @@ from Cryptodome.Util.Padding import unpad
 
 
 # DECRYPTION ###################################################################
+def decrypt(mode, data, password):
+    if (mode == 'CBC'):
+        return cbc_decrypt(data, password)
+    if (mode == 'GCM'):
+        return gcm_decrypt(data, password)
+    raise ValueError(f'unkown mode {mode}') ;
+
 def gcm_decrypt(data, password):
     salt = data[:16]
     nonce = data[16:32]
@@ -33,10 +41,18 @@ def cbc_decrypt(data, password):
     return unpad(cipher.decrypt(ciphertext), AES.block_size).decode()
 
 
+# OUTPUT #######################################################################
+def print_output(name, hostname, username, password):
+    if args.csv :
+        csv_out.writerow((name, hostname, username, password))
+    else:
+        print('Name: {}\nHostname: {}\nUsername: {}\nPassword: {}\n'.format(name, hostname, username, password))
+
 # MAIN #########################################################################
 parser = argparse.ArgumentParser(description = 'Decrypt mRemoteNG configuration files')
 parser.add_argument('config_file', type=str, help='mRemoteNG XML configuration file')
 parser.add_argument('-p', '--password', type=str, default='mR3m', help='Optional decryption password')
+parser.add_argument('--csv', default=False, action='store_true', help ='Output CSV format')
 args = parser.parse_args()
 
 with open(args.config_file, 'r') as f:
@@ -51,17 +67,27 @@ else:
     print('Unknown mode {}, implement it yourself or open a ticket'.format(mode[0]))
     sys.exit(1)
 
-nodes = re.findall('<Node .+/>', conf)
+# Extract and decrypt file data if FullFileEncryption is true
+full_encryption = re.findall('FullFileEncryption="([^"]*)"', conf)
+
+if full_encryption and (full_encryption[0] == 'true'):
+    cypher=base64.b64decode(re.findall('<.*>(.+)</mrng:Connections>', conf)[0]) 
+    conf=decrypt(mode, cypher, args.password.encode())
+
+nodes = re.findall('<Node .+?>', conf)
+
+if args.csv :
+    csv_out = csv.writer(sys.stdout, dialect='unix', quoting=csv.QUOTE_MINIMAL)
+    csv_out.writerow(('Name', 'Hostname', 'Username', 'Password'))
+
 for node in nodes:
+    name = re.findall(' Name="([^"]*)"', node)[0]
     username = re.findall(' Username="([^"]*)"', node)[0]
     hostname = re.findall(' Hostname="([^"]*)"', node)[0]
     data = base64.b64decode(re.findall(' Password="([^ ]*)"', node)[0])
-    if data == b'':
-        continue
+    password=""
+    if data != b'':
+        password=decrypt(mode, data, args.password.encode())
 
-    if mode == 'GCM':
-        password = gcm_decrypt(data, args.password.encode())
-    elif mode == 'CBC':
-        password = cbc_decrypt(data, args.password.encode())
+    print_output(name, hostname, username, password)
 
-    print('Hostname: {}\nUsername: {}\nPassword: {}\n'.format(hostname, username, password))
